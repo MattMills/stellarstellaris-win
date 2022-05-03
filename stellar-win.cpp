263e6bf95da4e140d257764184bc3915ab3ab38b
Matt Mills <mmills@2bn.net>
2022-05-02 22:49:08 -0600

    Add project files.

---
+++ stellar-win.cpp
@@ -0,0 +1,30 @@
+﻿// stellar-win.cpp : Defines the entry point for the application.
+//
+
+#include "stellar-win.h"
+#include <stdio.h>
+#include 
+
+using namespace std;
+
+int main()
+{
+	std::string cmd = "a = 7+11";
+
+	lua_State* L = luaL_newstate();
+
+
+	int r = luaL_dostring(L, cmd.c_str());
+	if (r == LUA_OK) {
+		lua_getglobal(L, "a");
+		if (lua_isnumber(L, -1)) {
+			float a_in_cpp = (float)lua_tonumber(L, -1);
+
+		}
+	} else {
+		std::string errormsg = lua_tostrong(L, -1);
+		std::cout << errormsg << std::endl;
+	}
+	cout << "Hello CMake." << endl;
+	return 0;
+}
