62796a40dba4e2670ee7612dd59332b3b99ced10
Matt Mills <mmills@2bn.net>
2022-08-26 08:15:46 -0600

    Fix bad function signature for CEvent::Execute

--- src/dll/cevent.cpp
+++ src/dll/cevent.cpp
@@ -7,8 +7,8 @@ intptr_t base_cevent_execute_ptr = 0x0;
 intptr_t base_steam_cevent_execute_ptr = 0x1402df460;
 intptr_t base_gog_cevent_execute_ptr = 0x1402dd7f0;
 
-void(*cevent_execute_trampoline)(void* ptr1);
-__declspec(noinline) void cevent_execute_payload(void* ptr1) {
+void(*cevent_execute_trampoline)(void* ptr1, void* arg2, void* arg3, void* arg4, void* arg5);
+__declspec(noinline) void cevent_execute_payload(void* ptr1, void* arg2, void* arg3, void * arg4, void* arg5) {
 	CLog& logger = *getLogger();
 	logger << "CEvent::Execute - ";
 #if DBG_PTR == 1
@@ -27,9 +27,9 @@ __declspec(noinline) void cevent_execute_payload(void* ptr1) {
 	logger << " EventID(" << str_event_id->c_str() << ")";
 	logger.endl();
 
-	void(*func_ptr)(void* ptr1);;
+	void(*func_ptr)(void* ptr1, void* arg2, void* arg3, void* arg4, void* arg5);
 	PopAddress(uint64_t(&func_ptr));
-	return func_ptr(ptr1);
+	return func_ptr(ptr1, arg2, arg3, arg4, arg5);
 
 }
 
@@ -43,6 +43,6 @@ void cevent_hook_init(enumPlatforms thisPlatform, intptr_t p_CApplication_Base, 
 	}
 
 	intptr_t this_cevent_execute_ptr = (intptr_t)p_CApplication_Base + (base_cevent_execute_ptr - base_augustus_ptr);
-	cevent_execute_trampoline = (void(*)(void* ptr1)) installHook((void*)this_cevent_execute_ptr, &cevent_execute_trampoline, cevent_execute_payload);
+	cevent_execute_trampoline = (void(*)(void* ptr1, void* arg2, void* arg3, void* arg4, void* arg5)) installHook((void*)this_cevent_execute_ptr, &cevent_execute_trampoline, cevent_execute_payload);
 	
 }
